4880144b814ef81c44463ce74fc14547f41aca68
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-09-28 00:25:24 +0200

    Funciona validación campos registro

--- signup/SignupController.js
+++ signup/SignupController.js
@@ -19,6 +19,7 @@ export class SignupController {
     //- El botón debe permanecer deshabilitado hasta que los campos estén rellenos
     const createUserInputElements= Array.from(this.signupElement.querySelectorAll('input'));
     const createUserButtonElement=this.signupElement.querySelector('#createUserButton');
+
     createUserInputElements.forEach(createUserInputElement => {
       createUserInputElement.addEventListener('input', ()=>{
         if (createUserInputElements.every(inputElement=>inputElement.value)) {
@@ -32,12 +33,22 @@ export class SignupController {
 
   validatePassword() {
     const passwordElement=this.signupElement.querySelector('#password');
-    console.log(passwordElement.value);
-    if (passwordElement.value.length<7) {
+    const minLength=6
+    if (passwordElement.value.length<minLength) {
       //mostrar error con pubSub
-      pubSub.publish(pubSub.TOPICS.NOTIFICATION_ERROR, 'La contraseña debe tener más de 6 caracteres');
+      pubSub.publish(pubSub.TOPICS.NOTIFICATION_ERROR, `La contraseña debe tener más de ${minLength} caracteres`);
     }
-  }
+
+    //Contraseña debe terner letras y números
+    const regExp = new RegExp(/^[a-zA-Z0-9]*$/);
+
+    if (regExp.test(passwordElement.value)) {
+      // hacemos cosas
+      //this.createUser();
+    } else {
+      pubSub.publish(pubSub.TOPICS.NOTIFICATION_ERROR, `La contraseña debe contener únicamente minúsculas, mayúsculas o números`);
+    };
+  };
 };
 
 //Comportamientos de nuestro formulario
